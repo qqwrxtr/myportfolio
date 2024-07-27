52d8040dd8b9bc0ca44b57e6d2474832479a85f3
qqwrxtr <teodorghirba1@gmail.com>
2024-07-27 19:05:03 +0300

    update for video faster loading

--- src/components/PhotoGalery/PhotoOverflow.jsx
+++ src/components/PhotoGalery/PhotoOverflow.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./photooverflow.css";
 
 const PhotoOverflow = ({ src, onClose }) => {
+    const [loaded, setLoaded] = useState(false);
+
     return (
         <div className="overflow">
             <div className="overflow-inner">
                 <button className="close-button" onClick={onClose}>X</button>
-                <video src={src} className="video-fluid scale" autoPlay loop muted />
+                {!loaded && <div className="placeholder">Loading...</div>}
+                <video
+                    src={src}
+                    className={`video-fluid scale ${loaded ? 'loaded' : ''}`}
+                    autoPlay
+                    loop
+                    muted
+                    onLoadedData={() => setLoaded(true)}
+                />
             </div>
         </div>
     );
