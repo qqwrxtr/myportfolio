0ca3c889ede5b70a0e845e6eac71b16ccc9c79e5
qqwrxtr <teodorghirba1@gmail.com>
2024-07-30 11:40:26 +0300

    adaptive update

--- src/components/Contact_Links/Contactlinks.jsx
+++ src/components/Contact_Links/Contactlinks.jsx
@@ -1,12 +1,31 @@
-import React from "react";
-import "./contactlinks.css"
-
-
+import React, { useState, useEffect } from "react";
+import "./contactlinks.css";
 
 const ContactLinks = (links) => {
+    const [minWidth, setMinWidth] = useState(window.innerWidth < 500 ? "180px" : "220px");
+
+    useEffect(() => {
+        const handleResize = () => {
+            setMinWidth(window.innerWidth < 500 ? "180px" : "220px");
+        };
+
+        window.addEventListener("resize", handleResize);
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+
     return (
         <a href={links.link}> 
-            <div className="btn-gradient width" style={{ width: links.width }}>
+            <div 
+                className="btn-gradient width" 
+                style={{  
+                    minWidth: minWidth,
+                    maxWidth: links.width,
+                    width: "100%",
+                }}
+            >
                 <div className="title_platform">
                     <div className="img_platform">
                         <img src={links.platicon} alt="" />
@@ -17,7 +36,7 @@ const ContactLinks = (links) => {
                 </div>
             </div>
         </a>
-     );
+    );
 }
- 
+
 export default ContactLinks;
